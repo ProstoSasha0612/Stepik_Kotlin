f20b87825eb237a53a61270f891ee2bec66e8cee
Alexandr Popko <alexandr.popko.2013@mail.ru>
2021-04-15 22:56:31 +0300

    Added 7 and 8 exercise

---
+++ src/chapter4/Exercise7.kt
@@ -0,0 +1,17 @@
+package chapter4
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return when {
+            year < other.year -> -1
+            year > other.year -> 1
+            month < other.month -> -1
+            month > other.month -> 1
+            dayOfMonth < other.dayOfMonth -> -1
+            dayOfMonth > other.dayOfMonth -> 1
+            else -> 0
+        }
+    }
+}
+
+fun compare(date1: MyDate, date2: MyDate) = date1 < date2
