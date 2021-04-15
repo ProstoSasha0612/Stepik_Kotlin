05f7016a4a2f6bc850759329ba5ba3761e788357
Alexandr Popko <alexandr.popko.2013@mail.ru>
2021-04-15 23:09:34 +0300

    ExtensionFunctions changed small mistake

--- src/chapter4/ExtensionFunctions.kt
+++ src/chapter4/ExtensionFunctions.kt
@@ -1,6 +1,6 @@
 package chapter4
 
-fun Int.r(): RationalNumber = RationalNumber(this.toInt(),0)
+fun Int.r(): RationalNumber = RationalNumber(this.toInt(),1)
 fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(first,second)
 
 data class RationalNumber(val numerator: Int, val denominator: Int)
